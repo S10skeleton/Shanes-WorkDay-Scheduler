0431ec8fb7932dfa25a8f02feec88e4d0d4fc6f3
Shane <s10skeleton@gmail.com>
2023-11-19 12:55:30 -0700

    updated features and README

--- README.md
+++ README.md
@@ -9,8 +9,13 @@ You'll need to use the [Day.js](https://day.js.org/en/) library to work with dat
 
 ## Description
 
-I created a custom app consisting of a daily scheduler with minor design upgrades including custom colors and a dark theme background. You can save individual appointments that will remain even if the page is closed until it is reopened. The scheduler will notify you with color changing backgrounds, green being future appointments, red being the current appointment, and gray appointments have been completed. The scheduler heading shows the current date and time at the top updating every 10 sec. Also within 30 minutes of your next appointment the appointment time will highlight red to indicate it is almost time to get ready. I also added a Clear All button at the bottom of the page so I can easily clear the day for a new one. 
+I created a custom app consisting of a daily scheduler with minor design upgrades including custom colors and a dark theme background. You can save individual appointments that will remain even if the page is closed until it is reopened. The scheduler will notify you with color changing backgrounds, green being future appointments, red being the current appointment, and gray appointments have been completed. The scheduler heading shows the current date and time at the top updating every 10 sec. Also within 10 minutes of your next appointment the appointment time will highlight red to indicate it is almost time to get ready. I also added a Clear All button at the bottom of the page so I can easily clear the day for a new one
+with a prompt for confirmation and a second prompt confirming reset. 
 
 ## Link to completed application
 
+https://s10skeleton.github.io/Shanes-WorkDay-Scheduler/
+
 ## Screen Shot 
+
+![Screenshot](./assets/Screenshot%202023-11-19%20125026.png)
--- assets/script.js
+++ assets/script.js
@@ -1,122 +1,98 @@
-          // Wrap all code that interacts with the DOM in a call to jQuery to ensure that
-          // the code isn't run until the browser has finished rendering all the elements
-          // in the html.
 $(function () {
-          // TODO: Add a listener for click events on the save button. This code should
-          // use the id in the containing time-block as a key to save the user input in
-          // local storage. HINT: What does `this` reference in the click listener
-          // function? How can DOM traversal be used to get the "hour-x" id of the
-          // time-block containing the button that was clicked? How might the id be
-          // useful when saving the description in local storage?
-          //
-  
-  // Created a function to display current date and time using dayjs, 
-  // and made sure current time updated every 10 Sec 
-  // added CSS style to underline the current date/time and make it stand out.  
-  
+  // Created a function to display current date and time using dayjs,
+  // and made sure current time updated every 10 Sec
+  // added CSS style to underline the current date/time and make it stand out.
+
   function updateDateTime() {
-    $('#currentDay').text(dayjs().format('dddd, MMMM D, h:mm A'));
-    }
+    $("#currentDay").text(dayjs().format("dddd, MMMM D, h:mm A"));
+  }
 
-    updateDateTime();
-    setInterval(updateDateTime, 10000); 
+  updateDateTime();
+  setInterval(updateDateTime, 10000);
 
-              // TODO: Add code to apply the past, present, or future class to each time
-          // block by comparing the id to the current hour. HINTS: How can the id
-          // attribute of each time-block be used to conditionally add or remove the
-          // past, present, and future classes? How can Day.js be used to get the
-          // current hour in 24-hour time?
-          //
-
-  // Created code to modify background color of scheduled events 
-  // to represent if the event is in the future or the past 
+  // Created code to modify background color of scheduled events
+  // to represent if the event is in the future, present, or the past
+  // unsure of cause but .replace('hour-', '') wouldnt work so I switched
+  // it to .substring(5) to remove potential conflicts and just count string length
   function updateHourlyBlocks() {
     var currentHour = dayjs().hour();
-    $('.time-block').each(function() {
-      var blockHour = parseInt($(this).attr('id').substring(5));
+    $(".time-block").each(function () {
+      var blockHour = parseInt($(this).attr("id").substring(5));
       if (blockHour < currentHour) {
-        $(this).addClass('past').removeClass('present future');
+        $(this).addClass("past").removeClass("present future");
       } else if (blockHour === currentHour) {
-        $(this).addClass('present').removeClass('past future');
+        $(this).addClass("present").removeClass("past future");
       } else {
-        $(this).addClass('future').removeClass('past present');
+        $(this).addClass("future").removeClass("past present");
       }
     });
   }
-// Set code to get current hour every 10 minuts 
+  // Set code to get current hour every 5 minuts
   updateHourlyBlocks();
 
-  setInterval(updateHourlyBlocks, 1000);
+  setInterval(updateHourlyBlocks, 50000);
 
-  
-
-  // Created Code to Save events and also pull events from local storage upon reloading 
+  // Created Code to Save events and also pull events from local storage upon reloading
   function saveEvent(hour, eventText) {
     localStorage.setItem(hour, eventText);
   }
 
   function loadEvents() {
-    $('.time-block').each(function() {
-      var hour = $(this).attr('id');
-      var eventText = localStorage.getItem(hour) || '';
-      $(this).find('.description').val(eventText);
+    $(".time-block").each(function () {
+      var hour = $(this).attr("id");
+      var eventText = localStorage.getItem(hour) || "";
+      $(this).find(".description").val(eventText);
     });
   }
 
   loadEvents();
 
-  $('.saveBtn').click(function() {
-    var hour = $(this).closest('.time-block').attr('id');
-    var eventText = $(this).siblings('.description').val();
+  $(".saveBtn").click(function () {
+    var hour = $(this).closest(".time-block").attr("id");
+    var eventText = $(this).siblings(".description").val();
     saveEvent(hour, eventText);
   });
-          
-          
-          
 
-  // Added a seperate function to highlight the next appointment time in red if less than 10 min 
+  // Added a seperate function to highlight the next appointment time in red if less than 10 min
+  // to give visual warning that the current hour is almost up
   function updateApptWarning() {
     var currentTime = dayjs();
-    $('.Appt').each(function() {
-      var apptTimeStr = $(this).attr('data-appt-time');
-      var apptTime = dayjs().hour(parseInt(apptTimeStr.split(':')[0])).minute(parseInt(apptTimeStr.split(':')[1]));
-  
-      if (currentTime.isBefore(apptTime) && apptTime.diff(currentTime, 'minute') <= 10) {
-        $(this).css('background-color', 'red');
+    $(".Appt").each(function () {
+      var apptTimeStr = $(this).attr("data-appt-time");
+      var apptTime = dayjs()
+        .hour(parseInt(apptTimeStr.split(":")[0]))
+        .minute(parseInt(apptTimeStr.split(":")[1]));
+
+      if (
+        currentTime.isBefore(apptTime) &&
+        apptTime.diff(currentTime, "minute") <= 10
+      ) {
+        $(this).css("background-color", "red");
       } else {
-        $(this).css('background-color', '');
+        $(this).css("background-color", "");
       }
-    })
+    });
   }
 
-   updateApptWarning();
-    setInterval(updateApptWarning, 60000);
-          
-          
-          
-          // TODO: Add code to get any user input that was saved in localStorage and set
-          // the values of the corresponding textarea elements. HINT: How can the id
-          // attribute of each time-block be used to do this?
-          //
-          
-          
-          // TODO: Add code to display the current date in the header of the page.
+  updateApptWarning();
+  setInterval(updateApptWarning, 60000);
 
-// Added a clear all button to reset for each new day if desired 
- // Clear all appointments
- function clearAllAppointments() {
-  localStorage.clear();
-  $('.description').val('');
-}
+  // Added a clear all button to reset for each new day 
+  // if desired, prompt for confirmation and prompt when cleared 
+  // Clear all appointments
+  function clearAllAppointments() {
+    var isConfirmed = confirm(
+      "Are you sure you want to clear all appointments?"
+    );
 
-$('.clearLocalStorage').click(function() {
-  clearAllAppointments();
-  alert('All appointments have been cleared!');
+    if (isConfirmed) {
+      localStorage.clear();
+      $(".description").val("");
+      alert("All appointments have been cleared!");
+    }
+  }
+
+  $(".clearLocalStorage").click(function () {
+    clearAllAppointments();
+  });
 });
-
-
-
-
-
-});
- 
